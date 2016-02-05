cf7570e5fa68268feb4e39d2896e28592dd49d43
phaninho <phaninho@live.fr>
2016-02-05 22:38:53 +0100

    update fet next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,69 +6,60 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 21:52:25 by stmartin          #+#    #+#             */
-/*   Updated: 2016/02/03 16:55:50 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/02/05 22:36:38 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "gnlinc.h"
 #include <stdio.h>
 
-int		main(int ac, char **av)
-{
-	int fd;
-	int	ret = 0;
-	char *line;
-
-	if (ac < 2)
-		return (1);
-	else
-	{
-		fd = open(av[1], O_RDONLY);
-		ret = get_next_line(fd, &line);
-	}
-	return (0);
-}
-
-void		read_file(int const fd, char **line, char **buff)
-{
-	static unsigned int	start = 0;
-	int					oct;
-	char				*str;
-	char				*tmp;
-	char				*ret;
-
-	str = NULL;
-	while ((oct = read(fd, *buff, BUFF_SIZE)))
-	{
-		printf ("pouf\n");
-		if (oct == -1)
-			return ;
-		printf("paff\n");
-		if ((ret = ft_strchr(*buff, '\n')))
-		{
-			*line = ft_strsub(*buff, start, (size_t)(ret - *buff));
-			start += (unsigned int)((ret - *buff) + 1);
-		}
-		printf("quand\n");
-		if (!str)
-			str = ft_strdup(*line);
-		else
-		{
-		printf("tin\n");
-			tmp = str;
-			str = ft_strjoin(str, *line);
-			free(tmp);
-		}
-	}
-}
 int			get_next_line(int const fd, char **line)
 {
-	char	*buff;
-	read_file(fd, line, &buff);
+	static char		*str = NULL;
+	char			*buff;
+	char			*tmp;
+	int				oct;
+	char			*chr;
+
 	*line = NULL;
+	if (!(buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (-1);
+	while ((oct = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[oct] = '\0';
+		printf("oct = %d\n", oct);
+		if (oct == -1)
+			return (-1);
+		if ((chr = ft_strchr(buff, '\n')))
+		{
+			printf("bonjour\n");
+			*line = ft_strjoin(*line, ft_strsub(buff, 0, (size_t)(chr - buff)));
+			str = ft_strdup(chr + 1);
+			return (1);
+		}
+		else
+		{
+			printf("monsieur\n");
+			tmp = *line;
+			*line = ft_strjoin(*line, buff);
+			printf("line = %s\n", *line);
+			if (tmp)
+				free(tmp);
+		}
+	}
 	return (0);
 }
 
+int		main(int ac, char **av)
+{
+	int		fd;
+	char	*line;
 
+	if (ac != 2 || (fd = open(av[1], O_RDONLY)) < 0)
+		return (-1);
+	get_next_line(fd, &line);
+	printf("line : %s\n", line);
+	return (0);
+}
 //ft_strjoin
 //ft_strsub
 //ft_strdup
