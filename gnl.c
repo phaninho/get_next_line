abacd3ad6c7cb3718cd98ca0c11f11228784122d
phaninho <phaninho@live.fr>
2016-02-17 03:01:25 +0100

    update, working version of gnl

--- gnl.c
+++ gnl.c
@@ -17,30 +17,29 @@ void		find_bsn(t_line *in, char **line)
 {
 	in->ret = 0;
 	in->chr = NULL;
-	/*if (in->str && (in->chr = ft_strchr(in->str, '\n')))
-	{
-		printf("goooooooooooooo");
-		in->tmp = *line;
-		*line = ft_strjoin(*line, ft_strsub(in->str, 0,
-					(size_t)(in->chr - in->str)));
-		if (in->tmp)
-			ft_strdel(&in->tmp);
-		ft_strdel(&in->str);
-		in->str = ft_strdup(in->str + 1);
-		in->ret = 1;
-	}*/
 	if (!in->ret && (in->chr = ft_strchr(in->buff, '\n')))
 	{
-		printf("goooooooooooooooooooooo");
-		in->tmp = *line;
-		*line = ft_strjoin(*line, ft_strsub(in->buff, 0, (size_t)(in->chr - in->buff)));
-		if (in->tmp)
-			ft_strdel(&in->tmp);
 		if (in->str)
+		{
+			in->tmp = *line;
+			*line = ft_strjoin(in->str, ft_strsub
+			(in->buff, 0, (size_t)(in->chr - in->buff)));
 			ft_strdel(&in->str);
-		in->str = ft_strdup(in->chr + 1);
-		in->ret = 1;
+			if (in->chr + 1)
+				in->str = ft_strdup(in->chr + 1);
+		}
+		else
+		{
+			in->tmp = *line;
+			*line = ft_strjoin(*line, ft_strsub
+			(in->buff, 0, (size_t)(in->chr - in->buff)));
+			if (in->tmp)
+				ft_strdel(&in->tmp);
+			if (in->chr + 1)
+				in->str = ft_strdup(in->chr + 1);
+		}
 	}
+	in->ret = 1;
 }
 
 int			check_str(t_line in, char *line)
@@ -49,20 +48,16 @@ int			check_str(t_line in, char *line)
 
 	tmp = NULL;
 	in.chr = NULL;
-	printf("entre str: %s\n", in.str);
 	if ((in.chr = ft_strchr(in.str, '\n')))
 	{
-		printf("regarde\n");
 		in.tmp = line;
-		line = ft_strjoin(line, ft_strsub(in.str, 0, ((size_t)(in.chr - in.str)
-		== 0 ? 1 : (size_t)(in.chr - in.str))));
+		line = ft_strjoin(line, ft_strsub(in.str, 0, (size_t)(in.chr - in.str)));
 		if (in.tmp)
 			ft_strdel(&in.tmp);
 		tmp = in.str;
 		if ((in.chr + 1))
 			in.str = ft_strdup(in.chr + 1);
 		ft_strdel(&tmp);
-		printf("pouk str: %s\n", in.str);
 		return (1);
 	}
 	return (0);
@@ -72,8 +67,6 @@ int			get_next_line(int const fd, char **line)
 {
 	static t_line			in;
 	in.ret = 0;
-//	if (*line)
-//		ft_strdel(line); // met line a NULL dans le retour
 	if (in.str && (check_str(in, *line) == 1))
 		return (1);
 	if (*line)
@@ -84,13 +77,13 @@ int			get_next_line(int const fd, char **line)
 	{
 		if (in.oct == -1)
 			return (-1);
+		if (in.oct == 0)
+			break;
 		in.buff[in.oct] = '\0';
-		printf("buff : %s\n",in.buff);
 		if (!(ft_strchr(in.buff, '\n')))
 		{
 			if (in.str)
 			{
-				printf("paffffffffff str: %s\n", in.str);
 				*line = ft_strjoin(in.str, in.buff);
 				ft_strdel(&in.str);
 			}
@@ -101,7 +94,6 @@ int			get_next_line(int const fd, char **line)
 				if (in.tmp)
 					ft_strdel(&in.tmp);
 			}
-			printf("-------------------------line: %s\n", *line);
 		}
 		else
 			find_bsn(&in, line);
@@ -123,9 +115,9 @@ int		main(int ac, char **av)
 
 	if (ac != 2 || (fd = open(av[1], O_RDONLY)) < 0)
 		return (-1);
-	while (i++ < 15)
+	while (i++ < 20)
 	{
-		(get_next_line(fd, &line) == -1) ? ft_putstr("error\n") : printf("-#%s#-\n", line);
+		(get_next_line(fd, &line) == -1) ? ft_putstr("error\n") : printf("%s\n", line);
 	}
 	return (0);
 }
